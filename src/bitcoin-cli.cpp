a3860bcc55b4ab86a9319845211a2cf5ce69139d
thk89 <thk89@hi.is>
2018-11-26 15:15:40 +0000

    var óvart á sendwithmessage branchinum

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -45,9 +45,8 @@ static bool AppInitRPC(int argc, char* argv[])
     return true;
 }
 
-int main(int argc, char* argv[])
-{
-    SetupEnvironment();
+int evaluateCommands(int argc, char *argv[]) {
+	int result = 0;
 
     try
     {
@@ -62,15 +61,77 @@ int main(int argc, char* argv[])
         return abs(RPC_MISC_ERROR);
     }
 
-    int ret = abs(RPC_MISC_ERROR);
+    result = abs(RPC_MISC_ERROR);
     try
     {
-        ret = CommandLineRPC(argc, argv);
+        result = CommandLineRPC(argc, argv);
     }
     catch (std::exception& e) {
         PrintExceptionContinue(&e, "CommandLineRPC()");
     } catch (...) {
         PrintExceptionContinue(NULL, "CommandLineRPC()");
     }
-    return ret;
+
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	int return_value = 0;
+	bool interactive_repeat = false;
+    SetupEnvironment();
+
+	//NOTE: * interactive json parameter, kannski táknað með -i
+	//		* robustness overhull
+	
+	if(argc > 1) {
+		if(strcmp(argv[1], "interactive") == 0) {
+			interactive_repeat = true;
+		}
+	}
+
+	if(!interactive_repeat) {
+		return_value = evaluateCommands(argc, argv);
+	}else {
+		printf("Interactive Mode(type quit to exit)\n\n");
+
+		while(interactive_repeat) {
+			printf("> ");
+			std::vector<std::string> arguments;
+			std::string line;
+			std::getline(std::cin, line);
+
+			std::istringstream lineStream(line);
+			std::string token;
+
+			//NOTE: gerum þetta þar sem fyrsta argumentið í argv er pwd
+			arguments.push_back("");
+
+			std::getline(lineStream, token, ' ');
+
+			//TODO: betri meðhöndlun á interactive specific argument-um
+			if(token != "quit") {
+				if(token != "") {
+					do {
+						//athuga hvort argument er -i og þá fara í eitthvað specific json string interactive mode
+						arguments.push_back(token);
+					}while(std::getline(lineStream, token, ' '));
+
+					unsigned int argument_count = 0;
+					char *c_arguments[arguments.size()];
+
+					for(; argument_count < arguments.size(); argument_count++) {
+						c_arguments[argument_count] = (char *)arguments[argument_count].c_str();
+					}
+
+					return_value = evaluateCommands(argument_count, c_arguments);
+				}
+			}else {
+				interactive_repeat = false;
+			}
+		}
+	}
+
+
+	return return_value;
 }
